e5e4c7808537a4688a6ce742503ca277c22e54c4
Aswani Nerella <aswani@getblueshift.com>
2017-10-20 00:37:42 -0700

    update api endpoint

--- BlueShift-iOS-SDK/BlueShiftRoutes.h
+++ BlueShift-iOS-SDK/BlueShiftRoutes.h
@@ -9,7 +9,7 @@
 #define BlueShift_iOS_SDK_BlueShiftRoutes_h
 
 
-#define kBaseURL                @"https://api.getblueshift.com/"
+#define kBaseURL                @"https://api.blueshift.com/"
 #define kRealTimeUploadURL      @"api/v1/event"
 #define kBatchUploadURL         @"api/v1/bulkevents"
 #define kPushEventsUploadURL    @"track"
